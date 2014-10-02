84b1b3acba26771c8c33c16a09ae95f61a2996d8
nacim <grindizer@gmail.com>
2014-10-02 14:44:46 +0200

    Added get account password policy call

--- boto/iam/connection.py
+++ boto/iam/connection.py
@@ -334,6 +334,7 @@ class IAMConnection(AWSQueryConnection):
             params['Marker'] = marker
         if max_items:
             params['MaxItems'] = max_items
+        import pdb;pdb.set_trace()
         return self.get_response('ListUsers', params, list_marker='Users')
 
     #
@@ -1523,3 +1524,7 @@ class IAMConnection(AWSQueryConnection):
         """
         params = {}
         return self.get_response('GetCredentialReport', params)
+
+    def get_account_password_policy(self):
+        params = {}
+        return self.get_response('GetAccountPasswordPolicy', params)
